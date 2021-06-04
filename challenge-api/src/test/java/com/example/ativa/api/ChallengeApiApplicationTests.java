3e2a488dcd563d38a7438e6413c31bac08e71530
unknown <leonardo.costa@EXT-ENG26.exsto.local>
2021-06-04 20:39:27 -0300

    feat: create project

---
+++ challenge-api/src/test/java/com/example/ativa/api/ChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ativa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
