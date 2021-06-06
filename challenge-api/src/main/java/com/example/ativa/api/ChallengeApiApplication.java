3328bc0b4d1f980e6be579d33eae77bf864f4a12
unknown <leonardo.costa@EXT-ENG26.exsto.local>
2021-06-05 21:55:11 -0300

    feat: https

--- challenge-api/src/main/java/com/example/ativa/api/ChallengeApiApplication.java
+++ challenge-api/src/main/java/com/example/ativa/api/ChallengeApiApplication.java
@@ -2,8 +2,12 @@ package com.example.ativa.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.ativa.api.config.property.ChallengeAtivaApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ChallengeAtivaApiProperty.class)
 public class ChallengeApiApplication {
 
 	public static void main(String[] args) {
